d037ff8e278cdd5ea00babfb5f5e58a549f64baa
Xing <zhaoxing8325@gmail.com>
2023-09-19 20:43:54 -0700

    Initial Commit

---
+++ ButtonModifier/ButtonModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonModifierApp.swift
+//  ButtonModifier
+//
+//  Created by XING ZHAO on 2023-09-19.
+//
+
+import SwiftUI
+
+@main
+struct ButtonModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
