540e3ec792f1ccd8fa426a2281b722edaea8b6f9
Xing <zhaoxing8325@gmail.com>
2023-09-19 20:49:20 -0700

    added a custom modifier

--- ButtonModifier/ContentView.swift
+++ ButtonModifier/ContentView.swift
@@ -7,13 +7,40 @@
 
 import SwiftUI
 
+struct DefaultButtonModifier: ViewModifier {
+    let color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .foregroundStyle(.white)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(color)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            .padding()
+    }
+}
+
+extension View {
+    func defaultButtonFormatting(color: Color) -> some View {
+        self.modifier(DefaultButtonModifier(color: color))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Button 1")
+                .defaultButtonFormatting(color: .red)
+                .onTapGesture {
+                    print("Tapped")
+                }
+        Text("Button 2")
+                .defaultButtonFormatting(color: .purple)
+        Text("Button 3")
+                    .defaultButtonFormatting(color: .blue)
         }
         .padding()
     }
